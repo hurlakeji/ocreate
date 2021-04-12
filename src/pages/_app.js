62858a464bd768b7e236318e1fc76a6195712749
iobami <ayobamialadenoye@yahoo.com>
2021-04-12 08:19:50 +0100

    clean up

---
+++ src/pages/_app.js
@@ -0,0 +1,19 @@
+import { Fragment } from 'react';
+
+import '../../public/css/bootstrap.min.css';
+import '../../public/css/font-awesome.min.css';
+import '../../public/css/prettyPhoto.css';
+import '../../public/css/animate.css';
+import '../../public/css/main.css';
+import '../../public/css/globals.css'
+import { Footer, Header } from '../components';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Fragment>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </Fragment>
+  );
+}
